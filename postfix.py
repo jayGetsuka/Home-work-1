edc1efb9f45d98d8bf2a4d2f97148831415948a0
JACHIN <71626667+jayGetsuka@users.noreply.github.com>
2020-12-11 12:28:35 +0700

    Add files via upload

--- postfix.py
+++ postfix.py
@@ -39,28 +39,37 @@ class ArrayStack(object):
     def size(self):
        return len(self.stk)
             
-def postfixEval(postfixExpr):
-    operandStack = ArrayStack()
-    tokenList = postfixExpr.split()
+def infixToPostfix(infixexpr):
+    prec = {}
+    prec["^"] = 4
+    prec["*"] = 3
+    prec["/"] = 3
+    prec["+"] = 2
+    prec["-"] = 2
+    prec["("] = 1
+    opStack = ArrayStack()
+    postfixList = []
+    tokenList = infixexpr.split()
 
     for token in tokenList:
-        if token in "0123456789":
-           operandStack.push(int(token))
+        if token in "ABCDEFGHIJKLMNOPQRSTUVWXYZ" or token in "0123456789":
+            postfixList.append(token)
+        elif token == '(':
+            opStack.push(token)
+        elif token == ')':
+            topToken = opStack.pop()
+            while topToken != '(':
+                postfixList.append(topToken)
+                topToken = opStack.pop()
         else:
-           operand2 = operandStack.pop()
-           operand1 = operandStack.pop()
-           result = calMath(token, operand1, operand2)
-           operandStack.push(result)
-    return operandStack.pop()
+            while (not opStack.isEmpty()) and \
+                (prec[opStack.peek()] >= prec[token]):
+                postfixList.append(opStack.pop())
+            opStack.push(token)
 
-def calMath(op, op1, op2):
-    if op == "*":
-        return op1 * op2
-    elif op == "/":
-        return op1 / op2
-    elif op == "+":
-        return op1 + op2
-    else:
-        return op1 - op2
+    while not opStack.isEmpty():
+        postfixList.append(opStack.pop())
+    return " ".join(postfixList)
 
-print(postfixEval("20 5 3 - / 5 *"))
+print(infixToPostfix("A - ( B / ( C - D ) * E + F ) ^ G"))
+
